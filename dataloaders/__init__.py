123c7bd4f8518ae0450d11d78cd3d190ba8fff73
Cookie <fanby2014@126.com>
2025-03-28 22:03:28 +0800

    feature: 完成迁移数据读取

---
+++ dataloaders/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .cifar100_subset_spliter import Cifar100_Spliter
+from .imagenet_r_subset_spliter import ImageNetR_Spliter
+from .utils import *
---
+++ dataloaders/utils.py
@@ -0,0 +1,61 @@
+import torch
+import numpy as np
+import os
+from PIL import Image
+from torchvision.transforms import transforms
+
+def accuracy(output, target, topk=(1,)):
+	### 返回的是正确的个数
+	maxk = min(max(topk), output.size()[1])
+	batch_size = target.size(0)
+	_, pred = output.topk(maxk, 1, True, True)
+	pred = pred.t()
+	correct = pred.eq(target.reshape(1, -1).expand_as(pred))
+
+	return [correct[:min(k, maxk)].reshape(-1).float().sum(0)  for k in topk],batch_size
+
+
+def global_distillation_loss(output,outputs):
+	mse = torch.nn.MSELoss(reduction='sum')
+	total_loss = 0
+	for i in outputs:
+		loss=mse(output,i)
+		total_loss +=loss
+	return total_loss
+
+def build_transform(is_train,input_size):
+	resize_im = input_size > 32
+	if is_train:
+		scale = (0.05, 1.0)
+		ratio = (3. / 4., 4. / 3.)
+		transform = transforms.Compose([
+			transforms.RandomResizedCrop(input_size, scale=scale, ratio=ratio),
+			transforms.RandomHorizontalFlip(p=0.5),
+			transforms.ToTensor(),
+		])
+		return transform
+
+	t = []
+	if resize_im:
+		size = int((256 / 224) * input_size)
+		t.append(
+			transforms.Resize(size, interpolation=3),  # to maintain same ratio w.r.t. 224 images
+		)
+		t.append(transforms.CenterCrop(input_size))
+	t.append(transforms.ToTensor())
+	return transforms.Compose(t)
+
+def process_file(fpath, folder, idx):
+	"""加载并转换单张图片，同时返回对应的标签"""
+	folder_path = os.path.join(fpath, folder)
+	X = []
+	y = []
+	for ims in os.listdir(folder_path):
+		img_path = os.path.join(folder_path, ims)
+		try:
+			img = np.array(Image.open(img_path).convert('RGB'))
+			X.append(img)
+			y.append(idx)
+		except Exception as e:
+			print(f"Error processing {img_path}: {e}")
+	return X, y
